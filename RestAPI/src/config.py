a305bd1f2f51f44e21eba63276338ede117b6852
Alexandr Fonov <FonovAD@Gmail.com>
2024-04-18 23:19:09 +0300

    chore config.py and database.py
    The connection type to the MongoDB database
    and configuration variables have been changed

--- RestAPI/src/config.py
+++ RestAPI/src/config.py
@@ -4,5 +4,8 @@ import os
 load_dotenv()
 
 MONGODB_URL = os.environ.get("MONGODB_URL")
-DB1_NAME = os.environ.get("DB1_NAME")
-DB1_COLL = os.environ.get("DB1_COLL")
+
+MESSAGE_DB = os.environ.get("MESSAGE_DB")
+MESSAGE_COLL = os.environ.get("MESSAGE_COLL")
+USER_DB = os.environ.get("USER_DB")
+USER_COLL = os.environ.get("USER_COLL")
--- RestAPI/src/database.py
+++ RestAPI/src/database.py
@@ -1,8 +1,9 @@
 from typing import AsyncGenerator
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from config import MONGODB_URL
+from config import MONGODB_URL, USER_DB, USER_COLL, MESSAGE_COLL, MESSAGE_DB
 
-async def get_async_mongo_session() -> AsyncGenerator[AsyncIOMotorClient, None]:
-    async with AsyncIOMotorClient(MONGODB_URL) as session:
-        yield session
+client = AsyncIOMotorClient(MONGODB_URL)
+
+UserDB = client.get_database(USER_DB).get_collection(USER_COLL)
+MessageDB = client.get_database(MESSAGE_DB).get_collection(MESSAGE_COLL)
