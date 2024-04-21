ef74416160a65014ca49b063c5a427fbe5d0c03d
Alexandr Fonov <FonovAD@Gmail.com>
2024-04-21 19:12:01 +0300

    chore Config.py and database.py
    Added User and Password configuration fields

--- RestAPI/src/config.py
+++ RestAPI/src/config.py
@@ -3,8 +3,12 @@ import os
 
 load_dotenv()
 
-MONGODB_URL = os.environ.get("MONGODB_URL")
 
+MONGODB_URL = os.environ.get("MONGODB_URL")
+MONGODB_PORT = os.environ.get("MONGODB_PORT")
+MONGODB_HOST = os.environ.get("MONGODB_HOST")
+MONGODB_USER = os.environ.get("MONGODB_USER")
+MONGODB_PASSWORD = os.environ.get("MONGODB_PASSWORD")
 MESSAGE_DB = os.environ.get("MESSAGE_DB")
 MESSAGE_COLL = os.environ.get("MESSAGE_COLL")
 USER_DB = os.environ.get("USER_DB")
--- RestAPI/src/database.py
+++ RestAPI/src/database.py
@@ -1,9 +1,29 @@
 from typing import AsyncGenerator
 from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
 
-from config import MONGODB_URL, USER_DB, USER_COLL, MESSAGE_COLL, MESSAGE_DB
+from config import (
+    USER_DB,
+    USER_COLL,
+    MESSAGE_COLL,
+    MESSAGE_DB,
+    MONGODB_HOST,
+    MONGODB_PASSWORD,
+    MONGODB_PORT,
+    MONGODB_USER,
+)
 
-client = AsyncIOMotorClient(MONGODB_URL)
 
-UserDB = client.get_database(USER_DB).get_collection(USER_COLL)
-MessageDB = client.get_database(MESSAGE_DB).get_collection(MESSAGE_COLL)
+MongoUrl = f"mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}:{MONGODB_PORT}"
+
+client = {}
+
+
+@asynccontextmanager
+async def dbInit(app: FastAPI):
+    client["client"] = AsyncIOMotorClient(MongoUrl)
+    client["MessageDB"] = client["client"].get_database(MESSAGE_DB).get_collection(MESSAGE_COLL)
+    client["UserDB"] = client["client"].get_database(USER_DB).get_collection(USER_COLL)
+    yield
+    client["client"].close()
