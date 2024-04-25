6c4948b099c32b56ff16159478ed461d19e2d8c6
Alexandr Fonov <FonovAD@Gmail.com>
2024-04-25 15:06:25 +0300

    feature getUser in Name/routes.py
    Added route for adding new users

--- RestAPI/src/Name/routes.py
+++ RestAPI/src/Name/routes.py
@@ -7,7 +7,7 @@ from Name.schemas import (
     Style,
     ResponseMessage,
 )
-from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.responses import JSONResponse, HTMLResponse, RedirectResponse
 import aiohttp
 from datetime import datetime
 
@@ -18,27 +18,51 @@ from database import client
 
 route = APIRouter(prefix="/Name", tags=["Name"])
 
+auth_data = "Yjg4MTQzMmUtNDAwMS00NDk0LThjOGUtNmU5ZWQ2YzQ4NDQ2OmQ4MWMxZGZiLTFmNGYtNDk5NS05OGQzLTBiMzYyYWJmNjk3OA=="
+
 
 @route.get(
     "/message_api",
     status_code=status.HTTP_200_OK,
 )
-async def message(UUID: str):
+async def message(UUID: str | None = None):
     MessageDB = client["MessageDB"]
     UserDB = client["UserDB"]
     if UUID:
-        result = MessageDB.find({"UUID": UUID})
+        result = MessageDB.find({"UUID": UUID}, {"_id": 0})
         messages = []
         async for elem in result:
             messages.append(elem)
+        print(messages)
         return {"result": messages}
-    return {"result": "Hello, JSON"}
+    return {"result": []}
+
+
+@route.post(
+    "/add_user",
+    status_code=status.HTTP_201_CREATED,
+)
+async def addUser():
+    UserDB = client["UserDB"]
+    userUUID = str(uuid.uuid4())
+    access_token = ""
+    expires_at = ""
+    try:
+        access_token, expires_at = await authGigaChat(userUUID=userUUID)
+        UserDB.insert_one(
+            {"UUID": userUUID, "access_token": access_token, "expires_at": expires_at}
+        )
+        return JSONResponse({"UUID": userUUID, "error": ""})
+    except Exception as e:
+        print(userUUID, e)
+        UserDB.insert_one({"UUID": userUUID, "access_token": "", "expires_at": ""})
+        return JSONResponse({"UUID": userUUID, "error": str(e)})
 
 
 @route.post(
     "/stat_hypothesis_api",
     response_model=ResponseStatHypothesis,
-    status_code=status.HTTP_201_CREATED,
+    status_code=status.HTTP_200_OK,
 )
 async def statHypothesis(
     request: RequestStatHypothesis,
@@ -46,58 +70,55 @@ async def statHypothesis(
     # Client = client["client"]
     MessageDB = client["MessageDB"]
     UserDB = client["UserDB"]
-    resp = ResponseStatHypothesis()
+    resp = ResponseStatHypothesis(UUID="", error="")
 
     # проверка на нового пользователя
     if request.UUID:
         resp.UUID = request.UUID
-        MessageDB.insert_one(
-            {
-                "user": True,
-                "UUID": resp.UUID,
-                "date": datetime.now(),
-                "Message": {
-                    "text": request.userData,
-                    "PlotType": None,
-                    "PlotData": {
-                        "X": None,
-                        "Y": None,
-                        "Z": None,
-                        "Styles": {
-                            "Color": None,
-                            "PlotSize": None,
-                        },
+    else:
+        resp.error = "Пользователя не существует. Перед вызовом /stat_hypothesis_api выполните выхов /add_user"
+        return resp
+    MessageDB.insert_one(
+        {
+            "user": True,
+            "UUID": resp.UUID,
+            "date": datetime.now(),
+            "Message": {
+                "text": request.userData,
+                "PlotType": None,
+                "PlotData": {
+                    "X": None,
+                    "Y": None,
+                    "Z": None,
+                    "Styles": {
+                        "Color": None,
+                        "PlotSize": None,
                     },
                 },
-                "Error": None,
-            }
-        )
-        userData = UserDB.find_one({"UUID": request.UUID})
-        access_token = userData["token"]
-        expires_at = userData["expires_at"]
+            },
+            "Error": None,
+        }
+    )
+    userData = await UserDB.find_one({"UUID": request.UUID})
+    access_token = userData["token"]
+    expires_at = userData["expires_at"]
 
-        # проверка не истек ли токен
-        if datetime.fromtimestamp(expires_at) < datetime.now():
-            try:
-                access_token, expires_at = authGigaChat(resp=resp)
-                UserDB.update_one(
-                    {"UUID": resp.UUID},
-                    {"access_token": access_token, "expires_at": expires_at},
-                )
-            except Exception as e:
-                print(resp.UUID, e)
-                resp.error = e
-                return resp
-    else:
-        resp.UUID = uuid.uuid4()
-        access_token, expires_at = authGigaChat(resp=resp)
-        UserDB.insert_one(
-            {"UUID": resp.UUID, "access_token": access_token, "expires_at": expires_at}
-        )
+    # проверка не истек ли токен
+    if datetime.fromtimestamp(expires_at) < datetime.now():
+        try:
+            access_token, expires_at = await authGigaChat(userUUID=resp.UUID)
+            UserDB.update_one(
+                {"UUID": resp.UUID},
+                {"access_token": access_token, "expires_at": expires_at},
+            )
+        except Exception as e:
+            print(resp.UUID, e)
+            resp.error = str(e)
+            return resp
 
     # делаем запрос к gigachain для расчета статистики.
     try:
-        data = getGigachainDataMock(request.userData)
+        data = await getGigachainDataMock(request.userData)
         resp.gigachainData = data[
             "gigachainData"
         ]  # Если получиться сделать формат ответа JSON, то можно просто никак не форматируя отправлять его на фронт
@@ -130,6 +151,7 @@ async def statHypothesis(
                 "Error": resp.error,
             }
         )
+        return resp
     except Exception as e:
         print(resp.UUID, e)
         resp.error = "Не удалось обработать запрос"
@@ -153,20 +175,19 @@ async def getGigachainDataMock(userMessage: str) -> dict[str]:
     }
 
 
-async def authGigaChat(resp: ResponseStatHypothesis) -> tuple[str, str]:
+async def authGigaChat(userUUID: str) -> tuple[str, str]:
     url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
-    payload = "scope=GIGACHAT_API_PERS"
-    auth_data = "Yjg4MTQzMmUtNDAwMS00NDk0LThjOGUtNmU5ZWQ2YzQ4NDQ2OmQ4MWMxZGZiLTFmNGYtNDk5NS05OGQzLTBiMzYyYWJmNjk3OA==  "
+    payload = "scope=GIGACHAT_API_CORP"
     async with aiohttp.ClientSession() as session:
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
             "Accept": "application/json",
-            "RqUID": resp.UUID,
+            "RqUID": str(userUUID),
             "Authorization": f"Basic {auth_data}",
         }
-        userData = await session.post(url=url, headers=headers, data=payload)
+        userData = await session.post(url=url, headers=headers, data=payload, ssl=False)
         if userData.status != 200:
-            raise Exception().add_note("Не удалось получить токен доступа к GigiChat")
-        access_token = userData.json()["access_token"]
-        expires_at = userData.json()["expires_at"]
-        return (access_token, expires_at)
+            raise Exception("Не удалось получить токен доступа к GigiChat")
+
+        jsonUserData = await userData.json()
+        return (jsonUserData["access_token"], jsonUserData["expires_at"])
